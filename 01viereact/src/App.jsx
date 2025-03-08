cae076c1242603a63fd0f048d4e429bc295549db
sandeep1162001 <sandeep1162001@gmail.com>
2025-03-08 18:04:28 +0530

    lecture 3 updates structure and flow of React

--- 01viereact/src/App.jsx
+++ 01viereact/src/App.jsx
@@ -1,8 +1,15 @@
+import Chai from "./chai"
+
 function App() {
 
   return (
-    <h1>Hello react from Sandeep via vite.</h1>
-  )
+    // <h1>Hello react from Sandeep via vite.</h1>
+    <>
+      <Chai />
+      <h3>We are coming for it.</h3>
+      <p>come fast.</p>
+    </>
+  );
 }
 
 export default App
---
+++ 01viereact/src/chai.jsx
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h2>Chai is ready and getting cold.</h2>;
+}
+export default Chai;
