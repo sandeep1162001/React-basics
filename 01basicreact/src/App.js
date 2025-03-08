436c86cbf96e22a5b42c59dc53fb439eb55187ce
sandeep1162001 <sandeep1162001@gmail.com>
2025-03-08 10:13:46 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello react from Sandeep.</h1>
+  );
+}
+
+export default App;
---
+++ 01viereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Hello react from Sandeep via vite.</h1>
+  )
+}
+
+export default App
